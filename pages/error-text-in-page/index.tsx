e18442fe3150993a2c496ef79abd98c0382388d3
Mike <mikesmallhelp@gmail.com>
2024-03-07 17:39:56 +0200

    Remove last change

--- pages/error-text-in-page/index.tsx
+++ pages/error-text-in-page/index.tsx
@@ -8,10 +8,6 @@ const IndexComponent = () => {
       <div>Error occurred!</div>
   );
 
-  useEffect(() => {
-    console.error("Hello! Something wrong!");
-  }, []);
-
   return (
     <>
       <PageTemplate title={'Test page - error-text-in-page'} content={PageContent} loading={loading} setLoading={setLoading} />
